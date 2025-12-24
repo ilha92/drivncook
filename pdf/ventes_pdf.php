ca61a694f3b088500862eb2a260f482c128063de
Ilias Hamel <iliashamel041@gmail.com>
2025-12-25 00:00:43 +0100

    mise ajours fonctionnel ventes

--- pdf/ventes_pdf.php
+++ pdf/ventes_pdf.php
@@ -1,18 +1,47 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../config/database.php';
+require_once __DIR__ . '/../src/models/Vente.php';
 
 use Dompdf\Dompdf;
-use Dompdf\Options;
 
-$dompdf = new Dompdf();
+// Récupération des ventes
+$ventes = Vente::getAll($pdo);
 
+// HTML du PDF
 $html = '
-<h1>Rapport des ventes</h1>
-<p>Test PDF fonctionnel</p>
-';
+<h2 style="text-align:center;">Rapport des ventes</h2>
+<table border="1" width="100%" cellpadding="5" cellspacing="0">
+<tr>
+    <th>Date</th>
+    <th>Franchise</th>
+    <th>Montant (€)</th>
+</tr>';
 
+$total = 0;
+
+foreach ($ventes as $v) {
+    $html .= "
+    <tr>
+        <td>{$v['date_vente']}</td>
+        <td>{$v['franchise']}</td>
+        <td>{$v['montant']} €</td>
+    </tr>";
+    $total += $v['montant'];
+}
+
+$html .= "
+<tr>
+    <td colspan='2'><strong>Total</strong></td>
+    <td><strong>$total €</strong></td>
+</tr>
+</table>
+";
+
+// Génération PDF
 $dompdf = new Dompdf();
 $dompdf->loadHtml($html);
 $dompdf->setPaper('A4', 'portrait');
 $dompdf->render();
-$dompdf->stream("ventes.pdf", ["Attachment" => false]);
+$dompdf->stream("rapport_ventes.pdf", ["Attachment" => false]);
+exit;
